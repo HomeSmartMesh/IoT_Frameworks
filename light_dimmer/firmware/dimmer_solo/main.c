369818e3fcfceb9ce08b59bb87bc3256a48d92cc
wassfila <wassim.filali@gmail.com>
2016-11-05 19:12:47 +0100

    dimmer solo, single channel with OnePulseMode tmr1_ch4 not working

---
+++ light_dimmer/firmware/dimmer_solo/main.c
@@ -0,0 +1,60 @@
+/*
+
+	main.c for 
+		IoT_Frameworks
+			\light_dimmer
+				\simple_monitor
+
+	started	05.11.2016
+	
+*/
+
+//Test Led is Port B Pin 5
+
+
+#include "uart.h"
+#include "clock_led.h"
+
+#include "light_dimmer_krida.h"
+
+#include <intrinsics.h>
+
+int main( void )
+{
+	
+	dimmer_init();//fast config - enables the PB4 interrupt
+
+    BYTE AliveActiveCounter = 0;
+
+    InitialiseSystemClock();
+
+
+    Initialise_TestLed_GPIO_B5();
+    Test_Led_Off();
+
+    uart_init();
+	
+    printf("\r\n__________________________________________________\n\r");
+    printf("IoT_Frameworks\\light_dimmer\\simple_monitor\\\n\r");
+
+	dimmer_set_level(0,70);//60us is the Sync shift + 10 to be in the positive section
+	dimmer_set_level(1,90);//60us is the Sync shift
+	
+    __enable_interrupt();
+
+    while (1)
+    {
+
+		printf("int count: ");
+		printf_uint(get_int_count());
+		printf_ln();
+
+		AliveActiveCounter++;//Why are you counting ?
+		
+		Test_Led_Off();
+		delay_1ms_Count(4900);
+		
+		Test_Led_On();
+		delay_1ms_Count(100);
+    }
+}
