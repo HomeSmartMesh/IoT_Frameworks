517b42436c06361f092717c699da9f5c9c8c0a7d
wassfila <wassim.filali@gmail.com>
2016-12-11 17:29:09 +0100

    added sensors websocket, added js viewer

--- sensors_logger/nodejs_server/sensors_server.js
+++ sensors_logger/nodejs_server/sensors_server.js
@@ -12,18 +12,54 @@ var WebSocketServer = require('ws').Server;
 
 var http = require('http');
 
-var server = http.createServer();
+var server_rf = http.createServer();
+var server_measures = http.createServer();
 
-var ws_serv = new WebSocketServer({server: server, path: '/rf_logger'});
+var ws_rf = new WebSocketServer({server: server_rf, path: '/rf_logger'});
+
+var ws_measures = new WebSocketServer({server: server_measures, path: '/measures'});
 
 
-
-
-ws_serv.on(	
+ws_rf.on(
 	'connection', 
 	function(ws) 
 	{
 		console.log('rf_logger is connected');
+
+		ws.on(
+		'message',
+		function(data, flags)
+		{
+			if (flags.binary) { return; }
+			console.log('rf_logger> ' + data);
+			ws.send('got it');
+			ws_measures.clients.forEach(
+			function each(client)
+			{
+				client.send('rf_logger ' + data);
+			}	);
+		}	);
+
+		ws.on	(
+		'close',
+		function()
+		{
+		  console.log('Closing the connection');
+		}		);
+
+		ws.on(
+		'error',
+		function(e) 
+		{
+			console.log('Error : ', e);
+		}	);
+	}	);
+//------------------------------------------------------------------------
+ws_measures.on(	
+	'connection', 
+	function(ws) 
+	{
+		console.log('measures client is connected');
 
 		ws.on(
 		'message',
@@ -49,6 +85,8 @@ ws_serv.on(
 		}	);
 	}	);
 
-server.listen(8126);
 
-console.log('Listening on port 8126...');
+server_rf.listen(8126);
+console.log('RF server Listening on port 8126...');
+server_measures.listen(4348);
+console.log('Measures server Listening on port 4348...');
---
+++ sensors_logger/web_nodejs_viewer/sensors_client.js
@@ -0,0 +1,98 @@
+/*
+ - Client ws websocket 
+*/
+
+$(function () {
+    "use strict";
+
+    // for better performance - to avoid searching in DOM
+    var content = $('#content');
+    var status = $('#status');
+
+    // my color assigned by the server
+    var myColor = false;
+    // my name sent to the server
+    var myName = false;
+
+    // if user is running mozilla then use it's built-in WebSocket
+    window.WebSocket = window.WebSocket || window.MozWebSocket;
+
+    // if browser doesn't support WebSocket, just show some notification and exit
+    if (!window.WebSocket) {
+        content.html($('<p>', { text: 'Sorry, but your browser doesn\'t '
+                                    + 'support WebSockets.'} ));
+        $('span').hide();
+        return;
+    }
+
+    // open connection
+    var connection = new WebSocket('ws://192.168.1.9:4348/measures');
+
+    connection.onopen = 
+	function()
+	{
+        // first we want users to enter their names
+        status.text('Connected');
+    };
+
+    connection.onerror = 
+	function(error) 
+	{
+        // just in there were some problems with conenction...
+        content.html($('<p>', { text: 'Sorry, but there\'s some problem with your '
+                                    + 'connection or the server is down.' } ));
+    };
+
+    // most important part - incoming messages
+    connection.onmessage = 
+	function(message)
+	{
+		addMessage(message.data);
+		console.log('received ', message.data);
+		/*
+        // try to parse JSON message. Because we know that the server always returns
+        // JSON this should work without any problem but we should make sure that
+        // the massage is not chunked or otherwise damaged.
+        try {
+            var json = JSON.parse(message.data);
+        } catch (e) {
+            console.log('This doesn\'t look like a valid JSON: ', message.data);
+            return;
+        }
+
+        // NOTE: if you're not sure about the JSON structure
+        // check the server source code above
+        if (json.type === 'color') { // first response from the server with user's color
+            myColor = json.data;
+            status.text(myName + ': ').css('color', myColor);
+            // from now user can start sending messages
+        } else if (json.type === 'message') { // it's a single message
+            addMessage(json.data.author, json.data.text,
+                       json.data.color, new Date(json.data.time));
+        } else {
+            console.log('Hmm..., I\'ve never seen JSON like this: ', json);
+        }*/
+    };
+
+    /**
+     * This method is optional. If the server wasn't able to respond to the
+     * in 3 seconds then show some error message to notify the user that
+     * something is wrong.
+     */
+    setInterval(
+		function()
+		{
+			if (connection.readyState !== 1) 
+			{
+				status.text('Error');
+			}
+		}
+		, 3000);
+
+    /**
+     * Add message to the chat window
+     */
+    function addMessage(message) {
+        content.prepend('<p>' + message + '</p>');
+    }
+});
