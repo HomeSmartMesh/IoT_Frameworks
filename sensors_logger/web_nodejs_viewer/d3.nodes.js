b6a8b713e31e7acb5f158c78ce5661e2a1f4bc41
wassfila <wassim.filali@gmail.com>
2017-01-07 18:33:57 +0100

    renamed as all sensors node, bullet temperature with gradient,

--- sensors_logger/web_nodejs_viewer/d3.nodes.js
+++ sensors_logger/web_nodejs_viewer/d3.nodes.js
@@ -11,10 +11,13 @@ var svgo = d3.select("body").append("svg")
 
 var Nodes = [
 	{ x: 100, y: 100, r: 20, color: "Blue", 
-		Temperature : 26, Light : 500
+		Temperature : 26, Light : 500,
+		Name : "LivingRoom"
 		},
 	{ x: 300, y: 100, r: 20, color: "Blue", 
-		Temperature : 26, Light : 500}
+		Temperature : 26, Light : 500,
+		Name : "BathRoom"
+		}
 ];
 
 var nmap = {6 : 0,
@@ -30,15 +33,25 @@ var colorTempScale = d3.scale.linear()
 	.range(coloursTempArray)
 	.interpolate(d3.interpolateHcl);
 
+var TempRange = [10,30];
 //Needed to map the values of the dataset to the color scale
 var colorTempInterpolate = d3.scale.linear()
-	.domain([10,30])
+	.domain(TempRange)
 	.range([0,1]);
 
 function TempToColor(temp)
 {
 	return colorTempScale(colorTempInterpolate(temp));
 }
+
+var temp_rect_min = 100;
+var temp_rect_height = 200;
+var temp_rect_max = -100;
+
+var TempToPos = d3.scale.linear()
+	.domain(TempRange)
+	.range([temp_rect_min,temp_rect_max]);
+
 
 console.log("coloursTempArray:",coloursTempArray);
 console.log("coloursTempRange:",coloursTempRange);
@@ -60,13 +73,19 @@ var defs = svgo.append("defs");
 
 //Append a linearGradient element to the defs and give it a unique id	
 var linearGradient = defs.append("linearGradient")
-    .attr("id", "linear-gradient");
+    .attr("id", "temp-color-gradient");
 
 linearGradient.selectAll("stop") 
     .data( colorTempScale.range() )
     .enter().append("stop")
     .attr("offset", function(d,i) { return i/(colorTempScale.range().length-1); })
-    .attr("stop-color", function(d) { return d; });
+    .attr("stop-color", function(d) { return d; })
+	;
+linearGradient	.attr("x1", "0%")
+				.attr("y1", "100%")
+				.attr("x2", "0%")
+				.attr("y2", "0%")
+				;
 
 var NodesGroups = svgo.selectAll("g").data(Nodes).enter().append("g");
 
@@ -78,24 +97,41 @@ NodesGroups.attr	("transform", function(d,i)
 										{
 											return "translate("	+d.x+","+d.y+")";
 										}
-					);
-TempNodesGroups.attr	("transform", "translate(-31,-23)");
-LightNodesGroups.attr	("transform", "translate(31,-23)");
-HumNodesGroups.attr	("transform", "translate(0,30)");
+					)
+				.append("text")	.text(function(d,i){return d.Name;})
+								.style("font-size", "16px")
+								.style("fill", "#011a4c")
+								.attr('x',-30)
+								.attr('y',90)
+				;
+
+TempNodesGroups.attr	("transform", "translate(-30,0)");
+LightNodesGroups.attr	("transform", "translate(40,-40)");
+HumNodesGroups.attr	("transform", "translate(40,40)");
 
 TempNodesGroups.append("circle")
 				.attr('r',30)
 				.attr('cx',0)
 				.attr('cy',0)
 				.style("fill", "White")
-				.style("stroke", "#000")
-				.style("stroke-width", "2px")
+				//.style("stroke", "#000")
+				//.style("stroke-width", "2px")
+				;
+TempNodesGroups.append("rect")
+				.attr('x',-50)
+				.attr('y',-temp_rect_min)
+				.attr('width',20)
+				.attr('height',temp_rect_height)
+				.style("fill", "url(#temp-color-gradient)")
+				//.style("stroke", "#000")
+				//.style("stroke-width", "2px")
 				;
 TempNodesGroups.append("text")
 				.text("Temperature")
 				.style("font-size", "12px")
+				.style("font-weight", "bold")
 				.style("fill", "#011a4c")
-				.attr('x',-22)
+				.attr('x',-25)
 				.attr('y',5)
 				;
 LightNodesGroups.append("circle")
@@ -138,9 +174,11 @@ function d3_SetTemperatureValue(id,Value)
 	if((id==6) || (id==7))
 	{
 		Nodes[nmap[id]].Temperature = Value;
-		TempGroupUpdate.select("text").text(function(d){return d.Temperature + " °C"});
+		TempGroupUpdate.select("text")	.text(function(d){return d.Temperature + " °C"})
+										.attr("y",function(d){return TempToPos(d.Temperature)+5});
 		//TempGroupUpdate.select("circle").style("fill",TempToColor(Value));
-		TempGroupUpdate.select("circle").style("fill",function(d){return TempToColor(d.Temperature)});
+		TempGroupUpdate.select("circle")	.style("fill",function(d){return TempToColor(d.Temperature)})
+											.attr("cy",function(d){return TempToPos(d.Temperature)});
 		//TempGroupUpdate.select("circle").style("fill","Blue");
 		//console.log(TempGroupUpdate.select("circle"));
 		//TempGroupUpdate.select("circle").style("fill","Blue"));
