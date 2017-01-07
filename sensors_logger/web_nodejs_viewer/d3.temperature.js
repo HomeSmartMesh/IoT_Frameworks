b0d1f74a9f5ad388bd58032db66b0e31ac084d09
wassfila <wassim.filali@gmail.com>
2017-01-07 14:53:47 +0100

    group nodes, group Temp, Group Light

--- sensors_logger/web_nodejs_viewer/d3.temperature.js
+++ sensors_logger/web_nodejs_viewer/d3.temperature.js
@@ -7,38 +7,61 @@ var svgo = d3.select("body").append("svg")
 	.style("background-color", "#dae0ea")
     .attr("width", w)
     .attr("height", 200)
-	.append("g")
     ;
 
-var Temp_Labels = [
-	{ id: 0, x: 10, y: 10, r: 20, color: "Blue" },
-	{ id: 0, x: 100, y: 10, r: 20, color: "Blue" }
+var Nodes = [
+	{ x: 100, y: 100, r: 20, color: "Blue", 
+		Temperature : 26, Light : 500
+		},
+	{ x: 300, y: 100, r: 20, color: "Blue", 
+		Temperature : 26, Light : 500}
 ];
 
-svgo.selectAll("g").data(Temp_Labels).enter().append("g")
-		.attr('class', 'cTemperature') 
-        .append("circle")
-        .attr("r", 20)
-		.attr('cx', function(d, i) {	return d.x;	})
-		.attr('cy', function(d, i) {	return d.y;	})
-		.style("fill", "purple")
-        .style("stroke", "#000")
-        .style("stroke-width", "2px")
-;
-/*
-var indexToVetPos = function(i)
-{
-    return (h * 0.1 + (h * 0.8) * (i) / 7);
-}
+var nmap = {6 : 0,
+			7 : 1};
 
-svgo.selectAll("g")
-	.transition().duration(500)
-	.attr("transform", function (d, i) { return "translate(" + indexToVetPos(i) + ",50)"; })
-;
-*/
-console.log(document.body.parentNode.innerHTML);
+var NodesGroups = svgo.selectAll("g").data(Nodes).enter().append("g");
+
+var TempNodesGroups = NodesGroups.append("g").attr("class","cTemp");
+var LightNodesGroups = NodesGroups.append("g").attr("class","cLight");
+
+NodesGroups.attr	("transform", function(d,i)
+										{
+											return "translate("	+d.x+","+d.y+")";
+										}
+					);
+
+TempNodesGroups.append("circle")
+				.attr('r',30)
+				.attr('cx',0)
+				.attr('cy',0)
+				.style("fill", "White")
+				.style("stroke", "#000")
+				.style("stroke-width", "2px")
+				;
+TempNodesGroups.append("text")
+				.text("Temperature")
+				.style("font-size", "12px")
+				.style("fill", "#011a4c")
+				.attr('x',-22)
+				.attr('y',5)
+				;
+LightNodesGroups.append("text")
+				.text("Light")
+				.style("font-size", "12px")
+				.style("fill", "#011a4c")
+				.attr('x',+22)
+				.attr('y',5)
+				;
+var TempGroupUpdate = svgo.selectAll("g.cTemp").data(Nodes);
+var LightGroupUpdate = svgo.selectAll("g.cLight").data(Nodes);
 
 function d3_SetTemperatureValue(id,Value)
 {
-	
+	if((id==6) || (id==7))
+	{
+		Nodes[nmap[id]].Temperature = Value;
+		//TempTextsUpdate.text(function(d){return d.Temperature + " °C"});
+		TempGroupUpdate.select("text").text(function(d){return d.Temperature + " °C"});
+	}
 }
