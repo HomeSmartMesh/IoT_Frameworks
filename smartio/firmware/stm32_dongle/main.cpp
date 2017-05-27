aa4bf4f2a04d5e2e4939024ded21b15d6c427cb5
wassfila <wassim.filali@gmail.com>
2017-05-27 13:05:31 +0200

    added two new projects dongle and RFPIO for STM32

---
+++ smartio/firmware/stm32_dongle/main.cpp
@@ -0,0 +1,74 @@
+#include "mbed.h"
+
+#include "rfmesh.h"
+#include "Servo.h"
+#include "suart.h"
+
+#define BOARD_IS_RF_DONGLE 1
+
+Serial   rasp(PB_10, PB_11, 115200);
+DigitalOut myled(PC_13);
+Ticker tick_call;
+//nRF Modules 1:Gnd, 2:3.3v, 3:ce,  4:csn, 5:sck, 6:mosi, 7:miso, 8:irq 
+#if BOARD_IS_RFPIO == 1
+//RfMesh mesh(&rasp,           PA_5,  PB_12, PB_13, PB_15, PB_14, PA_4);
+#elif BOARD_IS_RF_DONGLE ==1
+RfMesh mesh(&rasp,           PC_15, PA_4, PA_5,   PA_7,  PA_6,    PA_0);
+#endif
+
+suart com(&rasp);
+
+void the_ticker()
+{
+    myled = !myled;
+    
+}
+
+bool got_message = false;
+uint8_t payload[32];
+
+void uart_message_received(uint8_t *data)
+{
+    rasp.printf("OK message received with %d Bytes\n",data[0]);
+    uint8_t size = data[0];
+    for(int i=0;i<size;i++)
+    {
+        payload[i] = data[i];
+    }
+    got_message = true;
+}
+
+void rf_message_received(uint8_t *data,uint8_t size)
+{
+    rasp.printf("rf>Rx message (%d bytes) unused in dongle\n",size);
+}
+
+void init()
+{
+    rasp.printf("Hello PIO Dongle\n");
+
+    tick_call.attach(&the_ticker,1);
+
+    mesh.init();//left to the user for more flexibility on memory management
+
+    mesh.nrf.selectChannel(22);
+
+    mesh.attach(&rf_message_received,RfMesh::CallbackType::Message);
+
+    com.attach(uart_message_received);
+
+}
+
+int main() 
+{
+    init();
+
+    while(1) 
+    {
+        if(got_message)
+        {
+            mesh.nrf.start_transmission(payload,payload[0]);
+            got_message = false;
+        }
+    }
+}
---
+++ smartio/firmware/stm32_rfio/main.cpp
@@ -0,0 +1,107 @@
+#include "mbed.h"
+
+#include "rfmesh.h"
+#include "Servo.h"
+#include "suart.h"
+
+Serial   rasp(PB_10, PB_11, 115200);
+DigitalOut myled(PC_13);
+Ticker tick_call;
+//nRF Modules 1:Gnd, 2:3.3v, 3:ce,  4:csn, 5:sck, 6:mosi, 7:miso, 8:irq 
+//RFPIO Layout !!!!
+RfMesh mesh(&rasp,           PA_5,  PB_12, PB_13, PB_15, PB_14, PA_4);
+
+Servo ser_m11(PA_8);
+Servo ser_m12(PA_9);
+Servo ser_m13(PA_10);
+Servo ser_m14(PA_11);
+Servo ser_m21(PA_15);
+Servo ser_m22(PB_3);
+Servo ser_m31(PB_4);
+Servo *msr[7];
+
+DigitalOut pio_u1(PB_5);
+DigitalOut pio_u2(PB_6);
+DigitalOut pio_u3(PB_7);
+DigitalOut pio_u4(PB_8);
+DigitalOut pio_u5(PB_9);
+DigitalOut pio_u6(PB_1);
+DigitalOut pio_u7(PB_0);
+DigitalOut pio_u8(PA_7);
+DigitalOut *mio[8];
+
+void the_ticker()
+{
+    myled = !myled;
+    
+}
+
+void rf_message_received(uint8_t *data,uint8_t size)
+{
+    rasp.printf("rf>Rx message Handler : 0x");
+    for(int i = 0; i < size; i++)
+    {
+        rasp.printf(" %02x",data[i]);
+    }
+    rasp.printf("\r\n");
+}
+
+void init()
+{
+    msr[0] = &ser_m11;
+    msr[1] = &ser_m12;
+    msr[2] = &ser_m13;
+    msr[3] = &ser_m14;
+    msr[4] = &ser_m21;
+    msr[5] = &ser_m22;
+    msr[6] = &ser_m31;
+
+    mio[0] = &pio_u1;
+    mio[1] = &pio_u2;
+    mio[2] = &pio_u3;
+    mio[3] = &pio_u4;
+    mio[4] = &pio_u5;
+    mio[5] = &pio_u6;
+    mio[6] = &pio_u7;
+    mio[7] = &pio_u8;
+    
+
+    rasp.printf("Hello RFPIO\n");
+
+    tick_call.attach(&the_ticker,1);
+
+    mesh.init();//left to the user for more flexibility on memory management
+
+    rasp.printf("selectChannel(22)\n");
+    mesh.nrf.selectChannel(22);
+
+    mesh.attach(&rf_message_received,RfMesh::CallbackType::Message);
+
+}
+
+int main() 
+{
+    init();
+
+    while(1) 
+    {
+        for(int i=0;i<7;i++)
+        {
+            (*msr[i]) = 1;
+        }
+        for(int i=0;i<7;i++)
+        {
+            (*mio[i]) = 1;
+        }
+        wait(1.0);
+        for(int i=0;i<7;i++)
+        {
+            (*msr[i]) = 0;
+        }
+        for(int i=0;i<7;i++)
+        {
+            (*mio[i]) = 0;
+        }
+        wait(1.0);
+    }
+}
