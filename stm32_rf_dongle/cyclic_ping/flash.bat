50f4594ebbef779b1bd6bebefefebbd4da7f9560
wassfila <wassim.filali@gmail.com>
2017-11-05 18:08:12 +0100

    cyclic rf ping tester

---
+++ stm32_rf_dongle/cyclic_ping/flash.bat
@@ -0,0 +1,3 @@
+REM $env:Path += ";C:\Users\Wass\.platformio\packages\tool-stlink"
+SET PATH=%PATH%;C:\Users\Wass\.platformio\packages\tool-stlink
+st-flash write .\BUILD\BLUEPILL_F103C8\GCC_ARM\cyclic_ping.bin 0x08000000
---
+++ stm32_rf_dongle/cyclic_ping/main.cpp
@@ -0,0 +1,70 @@
+#include "mbed.h"
+
+#include "rfmesh.h"
+#include "protocol.h"
+#include "utils.h"
+
+//------------------------------------- CONFIG -----------------------------------------
+const uint8_t CHANNEL = 10;
+const uint8_t NODEID = 23;
+//--------------------------------------------------------------------------------------
+
+Serial   rasp(PB_10, PB_11, 115200);
+Proto    prf(&rasp);
+DigitalOut myled(PC_13);
+Ticker tick_call;
+//nRF Modules 1:Gnd, 2:3.3v, 3:ce,  4:csn, 5:sck, 6:mosi, 7:miso, 8:irq 
+RfMesh hsm(&rasp,1,           PC_15, PA_4, PA_5,   PA_7,  PA_6,    PA_0);
+
+uint8_t payload[32];
+
+void the_ticker()
+{
+    myled = !myled;
+}
+
+void init()
+{
+
+    uint8_t * p_UID = (uint8_t*) 0x1FFFF7E8;
+	
+	rasp.printf("pinger> U_ID: ");
+	print_tab(&rasp,p_UID,12);
+	rasp.printf("pinger> Node ID: %d\r",NODEID);
+
+    rasp.printf("pinger> Hi\n");
+
+    tick_call.attach(&the_ticker,1);
+
+    hsm.init(CHANNEL);
+    rasp.printf("pinger listening at channel %d\r",CHANNEL);
+    hsm.setNodeId(NODEID);
+
+	hsm.setRetries(0);
+	hsm.setAckDelay(0);
+	
+    hsm.print_nrf();
+	
+}
+
+int main() 
+{
+    init();
+
+    payload[rfi_size] = 30;
+    payload[rfi_pid] =  mesh::p2p::BIT7_DIRECTED    | mesh::p2p::BIT6_MSGACK | 
+                        mesh::p2p::BIT5_MESSAGE     | rf_pid_ping;
+    payload[rfi_src] = NODEID;
+    payload[rfi_dst] = 24;
+    for(int i = 4;i<30;i++)
+    {
+        payload[i] = i;
+    }
+
+
+    while(1) 
+    {
+        wait_ms(100);
+        hsm.send_msg(payload);
+    }
+}
