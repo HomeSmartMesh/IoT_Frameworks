35e45246de496a16b8f6e010729993854be795d1
wassfila <wassim.filali@gmail.com>
2017-12-09 13:40:31 +0100

    wroking complete status update and merge

--- raspi_mesh_server/web_react/src/StatusLogger.js
+++ raspi_mesh_server/web_react/src/StatusLogger.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
 import Websocket from 'react-websocket';
+import merge from 'deepmerge';
 
-
-function SensorsList(props){
-        const listitems = props.sensors.map((sensor) =>
-        <li key={sensor.type}>{sensor.type} : {sensor.value}</li>
-        );
-
-        return(
-            <div>
-                <ul>{listitems}</ul>
-            </div>
-        );
+function mergeSensors(map_old,map_new){
+    function overwriteMerge(destinationArray, sourceArray, options) {
+        return sourceArray
+    }
+    //Overwrite required in order not to grow arrays (verified)
+    let result = merge(map_old,map_new,{arrayMerge:overwriteMerge});
+    return result;
 }
-    
+
+function time_to_text(timestamp){
+    var t = new Date(timestamp * 1000);//as per javascript time in ms and c++ time_t in sec
+    return t.toLocaleTimeString() + " , " + t.toLocaleDateString();
+}
+
 function SensorsMap(props){
     const listitems = Object.keys(props.sensors).map((sensor) =>
-        <li key={sensor}>{sensor} : {props.sensors[sensor].Values[0]}</li>
+        <li key={sensor}>
+            {sensor} : {props.sensors[sensor].Values[0].toFixed(2)}  ({time_to_text(props.sensors[sensor].Times[0])})
+        </li>
         );
 
         return(
@@ -24,21 +28,6 @@ function SensorsMap(props){
                 <ul>{listitems}</ul>
             </div>
         );
-}
-
-function NodesList(props){
-    const listitems = props.updatelist.map((node) =>
-    <li key={node.NodeId}>
-        NodeId : {node.NodeId} 
-        <SensorsList sensors={node.sensors} /> 
-    </li>
-    );
-    return(
-            <div>
-                <ul>{listitems}</ul>
-            </div>
-    );
-
 }
 
 function NodesMap(props){
@@ -60,84 +49,23 @@ class StatusLogger extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            updatelist: [
-                {NodeId:"26",
-                    sensors:[
-                        {   type:"light",
-                            value:20
-                        },
-                        {   type:"temperature",
-                            value:30
-                        }
-                    ]
-                },
-                {NodeId:"8",
-                    sensors:[
-                        {   type:"light",
-                        value:21
-                        },
-                        {   type:"temperature",
-                            value:31
-                        }
-                    ]
-                },
-                {NodeId:"17",
-                    sensors:[
-                        {   type:"light",
-                        value:22
-                        },
-                        {   type:"temperature",
-                            value:32
-                        }
-                    ]
-                 },
-            ],
-            updatemap: {
-                "26":{
-                    light:{
-                        Times:[3333],
-                        Values:[20]
-                    },
-                    temperature:{
-                        Times:[3334],
-                        Values:[22]
-                    }
-                },
-                "28":{
-                    light:{
-                        Times:[3333],
-                        Values:[20]
-                    },
-                    temperature:{
-                        Times:[3334],
-                        Values:[22]
-                    }
-                }
-            }
+            updatemap: {}
         }
     }
 
     handleData(data) {
-        //console.log(data);
         let notification = JSON.parse(data);
-        console.log(notification.update);
+        //console.log(notification.update);
         this.setState( (prevState,props) => (
             {
-                updatemap: Object.assign(prevState.updatemap,notification.update)
+                updatemap: mergeSensors(prevState.updatemap,notification.update)
             }
         )
         );
     }
-    componentWillMount()
-    {
-        console.log("componentWillMount");
-        //console.log(this.state.update);
-    }
     render(){
     return(
         <div>
-            <NodesList updatelist={this.state.updatelist}/>
-            <hr/>
             <NodesMap updatemap={this.state.updatemap}/>
             <hr/>
             <Websocket  url='ws://10.0.0.12:4348/measures'
