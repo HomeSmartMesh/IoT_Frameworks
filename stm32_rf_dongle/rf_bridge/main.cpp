16f47a0c227447a72394d8f1a1d6895f3ae64d57
wassfila <wassim.filali@gmail.com>
2018-01-31 20:51:49 +0100

    added BME280 with first tests

--- stm32_rf_dongle/rf_bridge/main.cpp
+++ stm32_rf_dongle/rf_bridge/main.cpp
@@ -18,6 +18,8 @@
 #define USE_APDS_PROXIMITY 0
 #define USE_APDS_LIGHT 0
 
+#define USE_BME_SENSOR 1
+
 #define SEND_ALIVE  1
 #define BRIDGE_MODE 1
 #define RGB_DEMO 	0
@@ -30,7 +32,7 @@
 #define RF_BOARD_DONGLE 1
 #define RF_BOARD_PIO 	0
 //--------------------------------------------------------------------------------------
-Serial   rasp(PB_10, PB_11, 115200);
+Serial   pc(PB_10, PB_11, 115200);
 
 #if(USE_RGB_LED == 1)
 	#include "ws2812B.h"
@@ -45,15 +47,19 @@ Serial   rasp(PB_10, PB_11, 115200);
 #if (RF_BOARD_DONGLE == 1)
 	uint8_t spi_module = 1;
 	//nRF Modules 1:Gnd, 2:3.3v, 3:ce,  4:csn, 5:sck, 6:mosi, 7:miso, 8:irq 
-	RfMesh hsm(&rasp,spi_module,           PC_15, PA_4, PA_5,   PA_7,  PA_6,    PA_0);
+	RfMesh hsm(&pc,spi_module,           PC_15, PA_4, PA_5,   PA_7,  PA_6,    PA_0);
 #elif (RF_BOARD_PIO == 1)
 uint8_t spi_module = 2;
 #endif
 
+#if(USE_BME_SENSOR == 1)
+	#include "BME280.h"
+	BME280 bme(PB_7,PB_6);
+#endif
 
 DigitalOut debug_pio(PB_13);
 
-Proto    prf(&rasp);
+Proto    prf(&pc);
 DigitalOut myled(PC_13);
 Ticker tick_call;
 uint8_t led_count = 0;
@@ -70,6 +76,9 @@ uint8_t tab_send[32];
 uint8_t cmd_to_exec;//0 if no command to execute
 uint8_t cmd_params[32];
 uint8_t cmd_params_size;
+
+uint32_t ticks = 0;
+bool is_sametick = false;
 
 void handle_cmd(uint8_t cmd)
 {
@@ -92,21 +101,21 @@ void handle_cmd(uint8_t cmd)
 		break;
 		case rf::exec_cmd::get_channel :
 		{
-			rasp.printf("channel:%d\n",hsm.nrf.getChannel());
+			pc.printf("channel:%d\n",hsm.nrf.getChannel());
 		}
 		break;
 		case rf::exec_cmd::set_retries :
 		{
 			uint8_t nb_retries = cmd_params[0];
 			hsm.setRetries(nb_retries);
-			rasp.printf("nb_retries:%d\n",nb_retries);
+			pc.printf("nb_retries:%d\n",nb_retries);
 		}
 		break;
 		case rf::exec_cmd::set_ack_delay :
 		{
 			uint16_t ack_delay = ((uint16_t)cmd_params[0] <<8) + cmd_params[1];
 			hsm.setAckDelay(ack_delay);
-			rasp.printf("ack_delay_ms:%u\n",ack_delay);
+			pc.printf("ack_delay_ms:%u\n",ack_delay);
 		}
 		break;
 		case rf::exec_cmd::test_rf :
@@ -121,21 +130,56 @@ void handle_cmd(uint8_t cmd)
 			}
 			else
 			{
-				rasp.printf("target:%u;chan:%u;res:%u;nbping:%u\n",
+				pc.printf("target:%u;chan:%u;res:%u;nbping:%u\n",
 							target,channel,nb_success,nb_ping);
 			}
 		}
 		break;
 		default:
 		{
-			rasp.printf("unhandled cmd:0x%X\r\n",cmd);
+			pc.printf("unhandled cmd:0x%X\r\n",cmd);
 		}
 	}
 }
 
+#if(USE_BME_SENSOR == 1)
+void bme_send_temperature()
+{
+	float temp = bme.getTemperature();
+	if((temp > -20) && (temp<100))
+	{
+		int16_t temp_i = temp * 100;
+		hsm.broadcast_int16(rf::pid::temperature,temp_i);
+		pc.printf("Temperature:%d\n",temp_i);
+	}
+	else
+	{
+		pc.printf("Temperature out of range\n");
+	}
+}
 
+void bme_send_humidity()
+{
+	float hum = bme.getHumidity();
+	int16_t hum_i = hum * 100;
+	hsm.broadcast_int16(rf::pid::humidity,hum_i);
+	pc.printf("Humidity:%d\n",hum_i);
+}
+
+void bme_send_pressure()
+{
+	float press = bme.getPressure();
+	int16_t press_i = press * 100;
+	hsm.broadcast_int16(rf::pid::pressure,press_i);
+	pc.printf("Pressure:%d\n",press_i);
+}
+#endif
+
+//100 ms
 void the_ticker()
 {
+	ticks++;
+	is_sametick = false;
 	if(led_count != 0)
 	{
 		myled = 0;
@@ -161,7 +205,7 @@ void rf_message(uint8_t *data,uint8_t size)
 			uint8_t green = data[6];
 			uint8_t blue = data[7];
 			rgb_led.set(red,green,blue);
-			rasp.printf("R:%u;G:%u;B:%u\r\n",red,green,blue);
+			pc.printf("R:%u;G:%u;B:%u\r\n",red,green,blue);
 		}
 	#endif
 	
@@ -171,12 +215,12 @@ void rf_message(uint8_t *data,uint8_t size)
 		cmd_to_exec = data[rf::ind::p2p_payload];
 		is_rf_request = true;
 		rf_requester = data[rf::ind::source];
-		//rasp.printf("exec_cmd:0x%02X\n",cmd_to_exec);
+		//pc.printf("exec_cmd:0x%02X\n",cmd_to_exec);
 		for(uint8_t i = 0; i< cmd_params_size;i++)
 		{
 			cmd_params[i] = data[rf::ind::p2p_payload+1+i];
 		}
-		//print_tab(&rasp,cmd_params,cmd_params_size);
+		//print_tab(&pc,cmd_params,cmd_params_size);
 	}
 }
 
@@ -184,24 +228,24 @@ void init()
 {
 	uint8_t * p_UID = (uint8_t*) 0x1FFFF7E8;
 	
-	rasp.printf("stm32_bridge> U_ID: ");
-	print_tab(&rasp,p_UID,12);
-	rasp.printf("stm32_bridge> Node ID: %d\r",F_NODEID);
+	pc.printf("stm32_bridge> U_ID: ");
+	print_tab(&pc,p_UID,12);
+	pc.printf("stm32_bridge> Node ID: %d\r",F_NODEID);
 
 	tick_call.attach(&the_ticker,0.1);
 
 	hsm.init(F_CHANNEL);//left to the user for more flexibility on memory management
 	hsm.nrf.setMode(nrf::Mode::Rx);//not set by default as to check power consemption with hci
-	rasp.printf("mode:%d;channel:%d\n",hsm.nrf.getMode(),hsm.nrf.getChannel());
+	pc.printf("mode:%d;channel:%d\n",hsm.nrf.getMode(),hsm.nrf.getChannel());
 
     hsm.attach(&rf_sniffed,RfMesh::CallbackType::Sniff);
 	hsm.attach(&rf_message,RfMesh::CallbackType::Message);
 
 #if (BRIDGE_MODE == 1)
 	hsm.setBridgeMode();
-	rasp.printf("stm32_bridge> listening in bridge Mode\n");
+	pc.printf("stm32_bridge> listening in bridge Mode\n");
 #else
-	rasp.printf("stm32_bridge> Not in bridge Mode !!! Just a node\n");
+	pc.printf("stm32_bridge> Not in bridge Mode !!! Just a node\n");
 #endif
 
 	hsm.setNodeId(F_NODEID);
@@ -215,20 +259,21 @@ void init()
 	bool res;
 	#if(USE_APDS_SENSOR == 1)
 		res = gsensor.ginit();
-		rasp.printf("apds> ginit %u\r\n",res);
+		pc.printf("apds> ginit %u\r\n",res);
 	#endif
 	#if(USE_APDS_GESTURE == 1)
 		res = gsensor.enableGestureSensor();
-		rasp.printf("apds> Gesture sensor enable: %u\r\n",res);
+		pc.printf("apds> Gesture sensor enable: %u\r\n",res);
 	#endif
 	#if(USE_APDS_PROXIMITY == 1)
 		res = gsensor.enableProximitySensor();
-		rasp.printf("apds> Proximity sensor enable: %u\r\n",res);
+		pc.printf("apds> Proximity sensor enable: %u\r\n",res);
 	#endif
 	#if(USE_APDS_LIGHT == 1)
 		res = gsensor.enableLightSensor();
-		rasp.printf("apds> Light sensor enable: %u\r\n",res);
+		pc.printf("apds> Light sensor enable: %u\r\n",res);
 	#endif
+
 
 }
 
@@ -255,7 +300,7 @@ void apds_poll_gesture()
 		if(gest)
 		{
 			hsm.broadcast_byte(rf::pid::gesture,gest);
-			//rasp.printf("NodeId:%u;gesture:%u\r\n",F_NODEID,gest);//expected at rx gateway side
+			//pc.printf("NodeId:%u;gesture:%u\r\n",F_NODEID,gest);//expected at rx gateway side
 		}
 	}
 }
@@ -269,7 +314,7 @@ void apds_poll_proximity()
 	if(val > 25)
 	{
 		led_count = 1;
-		//rasp.printf("NodeId:%u;proximity:%u\n",F_NODEID,val);//slows down the 10 ms loop
+		//pc.printf("NodeId:%u;proximity:%u\n",F_NODEID,val);//slows down the 10 ms loop
 		#if(RGB_DEMO == 1)
 			uint8_t r = val;
 			uint8_t g = 0;
@@ -296,7 +341,7 @@ void apds_log_light_colors()
 		gsensor.readBlueLight(light_rgb[3]);
 		hsm.broadcast_light_rgb(light_rgb);
 		//expected at rx gateway side
-		//rasp.printf("NodeId:%u;light:%u;red:%u,green:%u;blue:%u\r\n",F_NODEID,light_rgb[0],light_rgb[1],light_rgb[2],light_rgb[3]);
+		//pc.printf("NodeId:%u;light:%u;red:%u,green:%u;blue:%u\r\n",F_NODEID,light_rgb[0],light_rgb[1],light_rgb[2],light_rgb[3]);
 
 		light_count = LOG_LOGHT_COUNT;//~10 sec
 	}
@@ -343,9 +388,23 @@ int main()
 			apds_poll_gesture();
 		#endif
 
+		#if(USE_BME_SENSOR == 1)
+		//100ms x 100 => 10 s
+		if(!is_sametick)
+		{
+			if(((ticks) % 100) == 0)
+			{
+				bme_send_temperature();
+				bme_send_humidity();
+				bme_send_pressure();
+			}
+			is_sametick = true;
+		}
+		#endif
+
 		if(hsm.nRFIrq.read() == 0)
 		{
-			rasp.printf("irq pin Low, missed interrupt, re init()\n");
+			pc.printf("irq pin Low, missed interrupt, re init()\n");
 			hsm.init(F_CHANNEL);
 			hsm.broadcast(rf::pid::reset);//notify if this issue happens
 		}
@@ -354,7 +413,7 @@ int main()
 			#if (SEND_ALIVE == 1)
 			hsm.broadcast(rf::pid::alive);
 			#endif
-			//rasp.printf("NodeId:%u;status:Alive\r\n",F_NODEID);//expected at rx gateway side
+			//pc.printf("NodeId:%u;status:Alive\r\n",F_NODEID);//expected at rx gateway side
 			alive_count = ALIVE_SEC;
 		}
 		else
